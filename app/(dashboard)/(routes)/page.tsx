dfd37535dc511319aee4259a7b7c70ab6cd40033
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-03 23:59:14 +0600

    feat: next js grouping, moved main page to the protected dashboard group

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>this is protected page</div>;
+};
+
+export default Home;
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div className="text-2xl text-green-700">Hello world</div>;
-}
