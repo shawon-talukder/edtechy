c6d45cccc9049573227541092861a5cb2240c411
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-06 20:21:17 +0600

    feat: if the user is teacher, then teacher routes work

--- app/(dashboard)/_components/SidebarRoutes.tsx
+++ app/(dashboard)/_components/SidebarRoutes.tsx
@@ -1,14 +1,27 @@
 "use client";
 
-import { Compass, Layout } from "lucide-react";
+import { BarChart, Compass, Layout, List } from "lucide-react";
+import { usePathname } from "next/navigation";
 import SidebarItem from "./SidebarItem";
 
 const guestRoutes = [
   { icon: Layout, label: "Dashboard", href: "/" },
   { icon: Compass, label: "Browse", href: "/search" },
 ];
+
+const teacherRoutes = [
+  { icon: List, label: "Courses", href: "/teacher/courses" },
+  { icon: BarChart, label: "Analytics", href: "/teacher/analytics" },
+];
+
 const SidebarRoutes = () => {
-  const routes = guestRoutes;
+  const pathName = usePathname();
+
+  // check wheather the page is teacher, means teacher logged in
+  const isTeacherPage = pathName?.includes("/teacher");
+
+  // if the user is teacher, then show routes for teacher
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
   return (
     <div className="flex flex-col w-full">
       {routes.map((route) => (
