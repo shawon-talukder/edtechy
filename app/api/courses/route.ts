bd8471810c84444bbed6653ff4a9ef0393f14d6a
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-07 14:16:55 +0600

    feat: creating courses with title

---
+++ app/api/courses/route.ts
@@ -0,0 +1,37 @@
+/*
+ *
+ *
+ ------->Title: 
+ ->Description: 
+ ------>Author: Shawon Talukder
+ -------->Date: 10/07/2023
+ *
+ *
+ */
+
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const { title } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized!", { status: 401 });
+    }
+
+    const course = await db.course.create({
+      data: {
+        userId,
+        title,
+      },
+    });
+
+    return NextResponse.json(course);
+  } catch (error) {
+    console.log("[COURSES]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
