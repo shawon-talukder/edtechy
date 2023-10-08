78a987b373b8355820c6a8532159551d5ef974f9
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-08 22:39:44 +0600

    feat: db category file create, prisma

---
+++ scripts/seed.ts
@@ -0,0 +1,27 @@
+const { PrismaClient } = require("@prisma/client");
+
+const db = new PrismaClient();
+
+async function main() {
+  try {
+    await db.category.createMany({
+      data: [
+        { name: "Computer Science" },
+        { name: "Music" },
+        { name: "Fitness" },
+        { name: "Photography" },
+        { name: "Accounting" },
+        { name: "Engineering" },
+        { name: "Filming" },
+      ],
+    });
+
+    console.log("success!");
+  } catch (error) {
+    console.log("Error seeding", error);
+  } finally {
+    await db.$disconnect();
+  }
+}
+
+main();
