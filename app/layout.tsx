eea3ff13709c4dccccad14dbe82452421fa62ce2
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-08 16:36:16 +0600

    fix: uploadthing uploader loading for a long time, prevent loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./globals.css";
 
 import ToasterProvider from "@/components/providers/toaster-provider";
 import { ClerkProvider } from "@clerk/nextjs";
-import "./globals.css";
+
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+
+import { ourFileRouter } from "@/app/api/uploadthing/core";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +27,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <ToasterProvider />
+          <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
           {children}
         </body>
       </html>
