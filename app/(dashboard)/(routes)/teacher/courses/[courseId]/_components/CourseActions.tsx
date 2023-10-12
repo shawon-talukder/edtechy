35869d1d4a42b7f8d79f37467bcf67da20660c47
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-12 19:19:13 +0600

    chore: publish, redo publishing, and delete the course

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/CourseActions.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import ConfirmModal from "@/components/modals/ConfirmModal";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+interface CourseActionsProps {
+  disabled: boolean;
+  isPublished: boolean;
+  courseId: string;
+}
+const CourseActions = ({
+  courseId,
+  disabled,
+  isPublished,
+}: CourseActionsProps) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  // handlers
+  const handlePublish = async () => {
+    try {
+      setIsLoading(true);
+      if (isPublished) {
+        await axios.patch(`/api/courses/${courseId}/unpublish`);
+
+        toast.success("course unpublished!");
+      } else {
+        await axios.patch(`/api/courses/${courseId}/publish`);
+
+        toast.success("course published!");
+      }
+    } catch (error) {
+      toast.error("Something went wrong!");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/courses/${courseId}`);
+
+      toast.success("Course deleted!");
+      router.refresh();
+      router.push("/teacher");
+    } catch (error) {
+      toast.error("Something went wrong!");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className="flex items-center gap-x-2">
+      <Button
+        variant={"outline"}
+        onClick={handlePublish}
+        size={"sm"}
+        disabled={disabled || isLoading}
+      >
+        {isPublished ? "Unpublish" : "Publish"}
+      </Button>
+      <ConfirmModal onConfirm={handleDelete}>
+        <Button size={"sm"} disabled={isLoading}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
+};
+
+export default CourseActions;
