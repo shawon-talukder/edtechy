d300a1d8526c885ef67070d36bc34ad0e5821003
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-12 19:10:07 +0600

    chore: delete a course permanently

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,6 +1,12 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
+import Mux from "@mux/mux-node";
 import { NextResponse } from "next/server";
+
+const { Video } = new Mux(
+  process.env.MUX_CLIENT_ID!,
+  process.env.MUX_CLIENT_SECRET!
+);
 
 export async function PATCH(
   req: Request,
@@ -46,5 +52,47 @@ export async function PATCH(
   } catch (error) {
     console.log("[COURSE_ID]", error);
     return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    // return error if user is not logged in
+    if (!userId) {
+      return new NextResponse("Unauthorized!", { status: 401 });
+    }
+
+    const { courseId } = params;
+
+    if (!courseId || courseId.length !== 36) {
+      return new NextResponse("Invalid Request!", { status: 400 });
+    }
+
+    const courseOwner = await db.course.findUnique({
+      where: { id: courseId, userId },
+      include: { chapters: { include: { muxData: true } } },
+    });
+
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized!", { status: 401 });
+    }
+
+    for (let ch of courseOwner.chapters) {
+      if (ch.muxData?.assetId) {
+        await Video.Assets.del(ch.muxData?.assetId);
+      }
+    }
+    
+    // delete the course
+    const deleteCourse = await db.course.delete({ where: { id: courseId } });
+
+    return NextResponse.json(deleteCourse);
+  } catch (error) {
+    console.log("COURSE_ID/DELETE", error);
   }
 }
