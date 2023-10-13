1c1f89bd354d3c711eea53667c9a2fa4474593af
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-13 17:34:45 +0600

    chore: dropdown menu, editing course button

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Course } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowDown, ArrowUp } from "lucide-react";
+import { ArrowDown, ArrowUp, MoreHorizontal, Pencil } from "lucide-react";
+import Link from "next/link";
 
 const getIcon = (direction: boolean) => {
   if (direction) {
@@ -44,5 +51,31 @@ export const columns: ColumnDef<Course>[] = [
   {
     accessorKey: "isPublished",
     header: "Status",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      // get course id
+      const { id } = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"ghost"} className="h-4 w-8 p-0">
+              <span className="sr-only">Open Menu</span>
+              <MoreHorizontal className="h-4 w-4 " />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <Link href={`/teacher/courses/${id}`}>
+              <DropdownMenuItem>
+                <Pencil className="h-4 w-4 mr-2" />
+                Edit
+              </DropdownMenuItem>
+            </Link>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
