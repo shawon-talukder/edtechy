6c9b294bde3269073cdd453ec9cfb1d7a017a659
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-13 17:04:30 +0600

    chore: soting data in the table asc and desc order

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -1,16 +1,45 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Course } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
+import { ArrowDown, ArrowUp } from "lucide-react";
+
+const getIcon = (direction: boolean) => {
+  if (direction) {
+    return <ArrowUp className="w-4 h-4 ml-2" />;
+  }
+  return <ArrowDown className="w-4 h-4 ml-2" />;
+};
 
 export const columns: ColumnDef<Course>[] = [
   {
     accessorKey: "title",
-    header: "Title",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant={"ghost"}
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Title
+          {getIcon(column.getIsSorted() === "asc")}
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "price",
-    header: "Price",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant={"ghost"}
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Price
+          {getIcon(column.getIsSorted() === "asc")}
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "isPublished",
