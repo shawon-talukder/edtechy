84c718e3bef4a1967c1413b66373649d44339581
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-13 16:33:23 +0600

    chore: display courses on teacher's course page

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,26 @@
-const CoursePage = () => {
-  return <div>This is for course player!</div>;
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+
+const CoursePage = async () => {
+  const { userId } = auth();
+
+  if (!userId) return redirect("/");
+
+  const courseData = await db.course.findMany({
+    where: { userId },
+    orderBy: { createdAt: "desc" },
+  });
+
+  return (
+    <div className="p-6">
+      <DataTable columns={columns} data={courseData} />
+    </div>
+  );
 };
 
 export default CoursePage;
