17060e032653da8a7e9aff18bf32a5a5447b272c
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-14 17:39:43 +0600

    chore: for small devices, search button is in top of course page

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,12 +1,18 @@
+import SearchInput from "@/components/SearchInput";
 import { db } from "@/lib/db";
 import Categories from "./_components/Categories";
 
 const SearchPage = async () => {
   const categories = await db.category.findMany({ orderBy: { name: "asc" } });
   return (
-    <div className="p-6">
-      <Categories items={categories} />
-    </div>
+    <>
+      <div className="px-6 pt-6 md:mb-0 block md:hidden">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories items={categories} />
+      </div>
+    </>
   );
 };
 
