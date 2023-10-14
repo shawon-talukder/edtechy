b016971671c8583ddd86edbf4bba262ae3720ccf
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-14 15:55:24 +0600

    chore: mount each category from categories, change on pathname

---
+++ app/(dashboard)/search/_components/CategoryItem.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
+
+import { IconType } from "react-icons";
+
+interface CategoryItemProps {
+  label: string;
+  value?: string;
+  icon?: IconType;
+}
+
+const CategoryItem = ({ label, value, icon: Icon }: CategoryItemProps) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  
+  const currentId = searchParams.get("categoryId");
+  const currentTitle = searchParams.get("title");
+
+  const isSelected = currentId === value;
+
+  const handleClick = () => {
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query: {
+          title: currentTitle,
+          categoryId: isSelected ? "null" : value,
+        },
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+
+    router.push(url);
+  };
+
+  return (
+    <button
+      className={cn(
+        "py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition",
+        isSelected && "border-sky-700 bg-sky-200/20 text-sky-800"
+      )}
+      type="button"
+      onClick={handleClick}
+    >
+      {Icon && <Icon size={20} />}
+      <div className="truncate">{label}</div>
+    </button>
+  );
+};
+
+export default CategoryItem;
