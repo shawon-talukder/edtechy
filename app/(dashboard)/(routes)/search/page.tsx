33078809d284bf9405896b4a57ff31ac360cc762
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-14 23:18:59 +0600

    chore: show all courses as a list

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,16 +1,39 @@
-import SearchInput from "@/components/SearchInput";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import getCourses from "@/actions/get-courses";
 import { db } from "@/lib/db";
+
+import CoursesList from "@/components/CoursesList";
+import SearchInput from "@/components/SearchInput";
 import Categories from "./_components/Categories";
 
-const SearchPage = async () => {
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+  // get all categories
   const categories = await db.category.findMany({ orderBy: { name: "asc" } });
+
+  // get all courses
+  const courses = await getCourses({ userId, ...searchParams });
+
   return (
     <>
       <div className="px-6 pt-6 md:mb-0 block md:hidden">
         <SearchInput />
       </div>
-      <div className="p-6">
+      <div className="p-6 space-y-6">
         <Categories items={categories} />
+        <CoursesList items={courses} />
       </div>
     </>
   );
---
+++ components/CoursesList.tsx
@@ -0,0 +1,34 @@
+import CourseCard from "@/components/CourseCard";
+
+import { CourseWithProgressAndCategory } from "@/actions/get-courses";
+
+interface CoursesListProps {
+  items: CourseWithProgressAndCategory[];
+}
+
+const CoursesList = ({ items }: CoursesListProps) => {
+  return (
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      {items.map((item) => (
+        <CourseCard
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          imageUrl={item.imageUrl!}
+          chaptersLength={item.chapters.length}
+          price={item.price!}
+          progress={item.progress}
+          category={item.Category?.name!}
+        />
+      ))}
+
+      {items.length === 0 && (
+        <div className="text-center text-sm text-muted-foreground mt-10">
+          No courses found
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CoursesList;
