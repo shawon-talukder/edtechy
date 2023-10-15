91306270f696ae152f8d2a4400ad06bcc0ae7e05
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-15 13:29:32 +0600

    chore: course page redirects to the first part of the chapter

---
+++ app/(course)/courses/[courseId]/page.tsx
@@ -0,0 +1,19 @@
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+const CoursePage = async ({ params }: { params: { courseId: string } }) => {
+  const { courseId } = params;
+  const course = await db.course.findUnique({
+    where: { id: courseId },
+    include: {
+      chapters: { where: { isPublished: true }, orderBy: { position: "asc" } },
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+  return redirect(`/courses/${course.id}/chapters/${course.chapters[0]?.id}`);
+};
+
+export default CoursePage;
