210bcacf57f1ceb951973ec1970065b22eaef59b
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-15 13:19:34 +0600

    fix: content is not showing in the middle, no courses found

--- components/CoursesList.tsx
+++ components/CoursesList.tsx
@@ -8,26 +8,27 @@ interface CoursesListProps {
 
 const CoursesList = ({ items }: CoursesListProps) => {
   return (
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      {items.map((item) => (
-        <CourseCard
-          key={item.id}
-          id={item.id}
-          title={item.title}
-          imageUrl={item.imageUrl!}
-          chaptersLength={item.chapters.length}
-          price={item.price!}
-          progress={item.progress}
-          category={item.Category?.name!}
-        />
-      ))}
-
+    <>
       {items.length === 0 && (
-        <div className="text-center text-sm text-muted-foreground mt-10">
+        <div className="text-center text-sm mt-10 bg-pink-200/20 border border-dashed py-6 border-pink-700 text-pink-800">
           No courses found
         </div>
       )}
-    </div>
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        {items.map((item) => (
+          <CourseCard
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            imageUrl={item.imageUrl!}
+            chaptersLength={item.chapters.length}
+            price={item.price!}
+            progress={item.progress}
+            category={item.Category?.name!}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
