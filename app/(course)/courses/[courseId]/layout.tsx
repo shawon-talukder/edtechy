893f938e8990bf82e195f14c12b5e5d243596e6b
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-15 12:32:56 +0600

    chore: new layout for course page

---
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -0,0 +1,58 @@
+import { redirect } from "next/navigation";
+import React from "react";
+
+import getProgress from "@/actions/get-progress";
+import { auth } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+
+import CourseNavbar from "./_components/CourseNavbar";
+import CourseSidebar from "./_components/CourseSidebar";
+
+const CourseLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { courseId: string };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: { id: params.courseId },
+    include: {
+      chapters: {
+        where: { isPublished: true },
+        include: { userProgress: { where: { userId } } },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  // get the user's progress count
+  const progressCount = await getProgress(userId, course.id);
+
+  return (
+    <div className="h-full">
+      <div className="h-80 md:pl-80 fixed inset-y-0 w-full z-50">
+        <CourseNavbar course={course} progressCount={progressCount} />
+      </div>
+      <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
+        <CourseSidebar course={course} progressCount={progressCount} />
+      </div>
+      <main className="md:pl-80 h-full">{children}</main>
+    </div>
+  );
+};
+
+export default CourseLayout;
