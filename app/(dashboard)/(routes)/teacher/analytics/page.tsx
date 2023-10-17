0f36399531d06952510b3300c88847b49a1e82d9
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-17 16:33:02 +0600

    chore: course sales, revenue summary

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,5 +1,26 @@
-const AnalyticsPage = () => {
-  return <div>AnalyticsPage</div>;
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { getAnalytics } from "@/actions/get-analytics";
+
+import DataCard from "./_components/DataCard";
+
+const AnalyticsPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+        <DataCard value={totalRevenue} label="Total Revenue" shouldFormat />
+        <DataCard value={totalSales} label="Total Sales" />
+      </div>
+    </div>
+  );
 };
 
 export default AnalyticsPage;
