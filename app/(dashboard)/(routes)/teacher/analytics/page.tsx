a178c9a8bf486d358fbd9b1082e1bdd40fbf6ac4
shawonTalukder <shawontalukder.study@gmail.com>
2023-10-17 23:52:08 +0600

    chore: show analytics through charts

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 
 import { getAnalytics } from "@/actions/get-analytics";
 
+import Chart from "./_components/Chart";
 import DataCard from "./_components/DataCard";
 
 const AnalyticsPage = async () => {
@@ -13,12 +14,14 @@ const AnalyticsPage = async () => {
   }
 
   const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
   return (
     <div className="p-6">
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
         <DataCard value={totalRevenue} label="Total Revenue" shouldFormat />
         <DataCard value={totalSales} label="Total Sales" />
       </div>
+      <Chart data={data} />
     </div>
   );
 };
